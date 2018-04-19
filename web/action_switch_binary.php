c519acc5d426301130502be08723b36563349112
Santi Pagola <santipagola@gmail.com>
2018-04-19 13:14:56 +0200

    show collapsible response message in php

--- web/action_switch_binary.php
+++ web/action_switch_binary.php
@@ -6,6 +6,13 @@
     text-align: center;
     margin: auto;
 }
+#myDIV {
+    width: 100%;
+    text-align: center;
+    background-color: #e8e8e8;
+    margin-top: 20px;
+    display: none;
+}
 </style>
 <body>
 <div class="content">
@@ -22,8 +29,40 @@ if (isset($_POST["binarySwitch"])) {
     $status = 0;
     echo "Switch will be turned off.";
 }
-$result = exec('/usr/bin/ozw-proxy-client -n21 -v'.$status);
+$result = shell_exec('/usr/bin/ozw-proxy-client -n21 -v'.$status.' 2>&1');
+$tmpfile = fopen(".status", "w");
+fwrite($tmpfile, $result);
+fclose($tmpfile);
 ?>
+
+<br><br>
+
+<fieldset>
+    <button id="myButton" onclick="showAndHide()">(Show Info)</button>
+    <div id="myDIV" display="none">
+<?php
+    $tmpfile = fopen(".status", "r");
+    echo fgets($tmpfile);
+    fclose($tmpfile);
+?>
+    </div>
+</fieldset>
+
+<br>
+
+<script>
+function showAndHide() {
+    var x = document.getElementById("myDIV");
+    var y = document.getElementById("myButton");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.innerHTML = "(Hide Info)";
+    } else {
+        x.style.display = "none";
+        y.innerHTML = "(Show Info)";
+    }
+}
+</script>
 
 <br>
 <br>
--- web/action_switch_multilevel.php
+++ web/action_switch_multilevel.php
@@ -6,6 +6,13 @@
     text-align: center;
     margin: auto;
 }
+#myDIV {
+    width: 100%;
+    text-align: center;
+    background-color: #e8e8e8;
+    margin-top: 20px;
+    display: none;
+}
 </style>
 <body>
 <div class="content">
@@ -17,13 +24,44 @@
 
 $level = $_POST["multilevelSwitch"];
 echo 'Will set multilevel dimmer to level: '.$level.'.';
-$result = exec('/usr/bin/ozw-proxy-client -n15 -v'.$level);
+$result = shell_exec('/usr/bin/ozw-proxy-client -n15 -v'.$level.' 2>&1');
+$tmpfile = fopen(".status", "w");
+fwrite($tmpfile, $result);
+fclose($tmpfile);
 ?>
 
-<br>
+<br><br>
+
+<fieldset>
+    <button id="myButton" onclick="showAndHide()">(Show Info)</button>
+    <div id="myDIV">
+<?php
+    $tmpfile = fopen(".status", "r");
+    echo fgets($tmpfile);
+    fclose($tmpfile);
+?>
+    </div>
+</fieldset>
+
 <br>
 
-<a href="/home.html">Back to main page</a>
+<script>
+function showAndHide() {
+    var x = document.getElementById("myDIV");
+    var y = document.getElementById("myButton");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.innerHTML = "(Hide Info)";
+    } else {
+        x.style.display = "none";
+        y.innerHTML = "(Show Info)";
+    }
+}
+</script>
+
+<br><br>
+
+<a href="/index.html">Back to main page</a>
 
 </div>
 </body>
