5b53d14f0bb49e898fa04c8353ea4b0fe055b82d
Santi Pagola <santipagola@gmail.com>
2018-04-19 11:49:42 +0200

    Change flag for serial device to avoid confict with Qt5 -platform flag

--- server/main.cpp
+++ server/main.cpp
@@ -17,7 +17,7 @@ using std::endl;
 
 static struct option  long_options[] =
 {
-    {"port"         , required_argument , 0             , 'p'   },
+    {"tty"          , required_argument , 0             , 'd'   },
     {"config"       , required_argument , 0             , 'c'   },
     {"help"         , no_argument       , 0             , 'h'   },
     {"silent"       , no_argument       , 0             , 's'   },
@@ -34,8 +34,8 @@ void help(QString progname) {
     cout << left << setw(width - internal_width) << "-c --config" << setw(internal_width) << "<dir>"
         << "Choose where the config files are stored." << endl;
     cout << left << setw(width) << "-h --help" << "Show this help and exit." << right << endl;
-    cout << left << setw(width - internal_width) << "-p --port" << setw(internal_width) << "<tty>"
-        << "Choose USB serial port." << endl;
+    cout << left << setw(width - internal_width) << "-d --tty" << setw(internal_width) << "<tty>"
+        << "Choose USB serial device port." << endl;
     cout << left << setw(width) << "-s --silent" << "Don't output anything to console." << endl;
     cout << left << setw(width) << "-x --gui" << "Run the daemon graphically." << endl;
 }
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
     QString port{ "/dev/ttyACM0" };
     QString config{ "./config/" };
 
-    while (( c = getopt_long(argc, argv, "c:hp:sx", long_options, &optindex)) != -1)
+    while (( c = getopt_long(argc, argv, "c:hd:sx", long_options, &optindex)) != -1)
     {
         switch (c)
         {
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
         case 'h':
             help( QString::fromStdString( std::string{argv[0]}) );
             return 0;
-        case 'p':
+        case 'd':
             port = QString::fromStdString( std::string{optarg} );
             break;
         case 's':
